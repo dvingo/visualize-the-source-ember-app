8a01768e4667603b39ae3caa44ee350ed7d294f9
Dan Vingo <danvingo@gmail.com>
2014-04-20 11:52:07 -0400

    Started to get highlighting working

--- ember_app/app/controllers/vis.js
+++ ember_app/app/controllers/vis.js
@@ -7,16 +7,26 @@ export default Em.ObjectController.extend({
 
   actions: {
     showFile: function(file) {
+      var self = this, newContent;
       if (file.type === 'file') {
         if (this.get('useLeftPane')) {
           this.set('selectedFileOneName', file.name);
-          //this.set('selectedFileOneContent', Em.String.htmlSafe(file.content.replace(/\n/g, '<br>')));
-          this.set('selectedFileOneContent', file.content);
+          //Em.run(function() {
+            //$('.file.left pre code').removeClass();
+          newContent = hljs.highlight('javascript', file.content);
+          self.set('selectedFileOneContent', newContent.value);
+
+            //$('.file.left pre code').each(function(i, e) { hljs.highlightBlock(e); });
+          //});
           this.toggleProperty('useLeftPane');
         } else {
           this.set('selectedFileTwoName', file.name);
-          //this.set('selectedFileTwoContent', Em.String.htmlSafe(file.content.replace(/\n/g, '<br>')));
-          this.set('selectedFileTwoContent', file.content);
+          //Em.run(function() {
+            //$('.file.right pre code').removeClass();
+          newContent = hljs.highlight('javascript', file.content);
+          self.set('selectedFileTwoContent', newContent.value);
+            //$('.file.right pre code').each(function(i, e) { hljs.highlightBlock(e); });
+          //});
           this.toggleProperty('useLeftPane');
         }
       }
